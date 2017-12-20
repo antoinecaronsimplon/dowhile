6b5d9bb5f2e85582aac710304d23e6fdb02b1ee6
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-20 09:50:15 +0100

    progress

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 (function main(){
-	
+	$('#idbtn').on('click', function(e){
+		testWhile();
+	});
+	function testWhile() {
+		var chaine;
+		do {
+			chaine=prompt("Entrez une chaine de caractères contenant le caractère\"");
+		}
+		while (chaine.indexOf("\"")==-1)
+		alert("la chaine entrée est \""+chaine+"\"");
+	}
 })();
